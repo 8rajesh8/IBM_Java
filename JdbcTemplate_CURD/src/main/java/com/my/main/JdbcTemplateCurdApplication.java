d701c91a906a143f921388b36c7485e65485f006
Fire Bender <Fire Bender@172.22.136.215>
2020-04-13 22:23:39 +0530

    first commit

---
+++ JdbcTemplate_CURD/src/main/java/com/my/main/JdbcTemplateCurdApplication.java
@@ -0,0 +1,13 @@
+package com.my.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcTemplateCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcTemplateCurdApplication.class, args);
+	}
+
+}
---
+++ JdbcTemplate_CURD/src/test/java/com/my/main/JdbcTemplateCurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcTemplateCurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
